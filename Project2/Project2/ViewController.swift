cb063888dc9ab0cb7fd23367e78121c0a9c431b1
koala panda <koakoa8823@protonmail.com>
2023-03-18 17:51:33 +0900

    プロジェクト2（国旗当てゲーム）新規作成
    Hacking with Swift day19にてプロジェクトを作成した。

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by koala panda on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
