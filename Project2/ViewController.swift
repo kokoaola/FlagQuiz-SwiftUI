5eccad8f239ee27659306699dde968658b4253c9
koala panda <koakoa8823@protonmail.com>
2023-03-19 13:34:46 +0900

    旗のレイアウトの修正をした。

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -10,22 +10,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var button1: UIButton!
-    
     @IBOutlet var button2: UIButton!
-    
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
+        
+        //button1.layer.backgroundColor = UIColor.red.cgColor
+        //button1.layer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        countries.shuffle()
         askQuestion()
         
     }
@@ -35,12 +43,14 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
     
     
-    
-    var countries = [String]()
-    var score = 0
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
     
 }
 
